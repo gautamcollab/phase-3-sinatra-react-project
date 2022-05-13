29752da4a987e3a6746783d001a3737e4bafcbba
Gautam Sinha <gautam.sinha@live.com>
2022-05-13 13:57:38 -0400

    add char functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,25 @@ class ApplicationController < Sinatra::Base
     characters.to_json 
   end 
 
+  post "/characters" do 
+    char = Character.create(
+        character: params[:character],
+        topspin: params[:topspin],
+        slicing: params[:slicing], 
+        flat: params[:flat],
+        approach: params[:approach],
+        baseline: params[:baseline],
+        doubletapflat: params[:doubletapflat],
+        doubletapslice: params[:doubletapslice],
+        max_speed: params[:max_speed],
+        acceleration: params[:acceleration],
+        pivot: params[:pivot],
+        horizontal: params[:horizontal],
+        knockback_type: params[:knockback_type],
+        lunge_recovery: params[:lunge_recovery],
+        knockback_reduction: params[:knockback_reduction]
+        )
+    char.to_json
+end
+
 end
